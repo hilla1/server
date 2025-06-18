835867426c91f3e4516fb08f425e55100abacc68
Kipkirui Hillary <kiph62@gmail.com>
2025-06-18 23:11:35 +0300

    Fix cors error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['https://techwithbrands.com/'];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
