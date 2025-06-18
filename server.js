e638b3e954fac2d1c08ec5453fe466a977d4d26e
Kipkirui Hillary <kiph62@gmail.com>
2025-06-18 23:12:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ['https://techwithbrands.com/'];
+const allowedOrigins = ['https://techwithbrands.com'];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
